d84e1f101d29c40119ee7ed4806719c507be5a11
sw1tcherrr <sw1tcherrr@mail.ru>
2022-11-27 11:57:10 +0300

    input

--- main.cpp
+++ main.cpp
@@ -12,12 +12,25 @@ namespace bgi = boost::geometry::index;
 int main() {
     using point = bg::model::point<double, 2, bg::cs::cartesian>;
 	using box = bg::model::box<point>;
-    using polygon =  bg::model::polygon<point, false, false>; // ccw, open polygon
+    using polygon =  bg::model::polygon<point>;
 
+	std::string test_num = "01";
+	std::string test_name = std::string("../tests/") + test_num + ".txt";
+	std::string res_name = std::string("../") + test_num + ".txt";
+	std::freopen(test_name.data(), "r", stdin);
+    std::freopen(res_name.data(), "w", stdout);
 
+	int n;
+	double c1;
+	double c2;
+	std::cin >> n >> c1 >> c2;
 
-    std::vector<point> result;
-    rtree.query(bgi::intersects<polygon>(poly), std::back_inserter(result));
+	polygon poly;
+	for (int i = 0; i < n; ++i) {
+		double x, y;
+		std::cin >> x >> y;
+		poly.outer().push_back(point(x, y));
+	}
 
     for (point p : result) {
         std::cout << bg::wkt<point>(p) << std::endl;
