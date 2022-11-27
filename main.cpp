14a6939a7d5356d3cc6963ff565ffb57addede9b
gashnya <ebmebm14@gmail.com>
2022-11-27 12:48:30 +0300

    aboba (2) + polygon

--- main.cpp
+++ main.cpp
@@ -30,7 +30,7 @@ int main() {
     box bbox = bg::return_envelope<box>(poly);
     double bbox_val = 1 + bg::area(bbox) * c1;
 
-    bgi::rtree<box, bgi::rstar<16, 4>> rtree;
+    bgi::rtree<polygon, bgi::rstar<16, 4>> rtree;
 
     double bbox_h = bbox.max_corner().get<1>() - bbox.max_corner().get<1>();
     double bbox_w = bbox.max_corner().get<0>() - bbox.max_corner().get<0>();
@@ -48,8 +48,12 @@ int main() {
 
     for (int i = 0; i < h_piece_cnt; ++i) {
         for (int j = 0; j < w_piece_cnt; ++j) {
-            box b(min_corner, max_corner);
-            rtree.insert(b);
+            point corner2 = min_corner;
+            bg::add_point(corner2, point(0, h_piece));
+            point corner4 = min_corner;
+            bg::add_point(corner4, point(w_piece, 0));
+            polygon p = {{min_corner, corner2, max_corner, corner4}};
+            rtree.insert(p);
 
             bg::add_point(min_corner, point(w_piece, 0));
             bg::add_point(max_corner, point(w_piece, 0));
@@ -59,10 +63,10 @@ int main() {
         bg::add_point(max_corner, point(0, h_piece));
     }
 
-    std::vector<point> result_i;
+    std::vector<polygon> result_i;
     size_t intersects_cnt = rtree.query(bgi::intersects<polygon>(poly), std::back_inserter(result_i));
 
-    std::vector<point> result_w;
+    std::vector<polygon> result_w;
     size_t within_cnt = rtree.query(bgi::within<polygon>(poly), std::back_inserter(result_w));
 
     double sum = within_cnt * (1 + area_piece * c1) + (intersects_cnt - within_cnt) * (1 + area_piece * c2);
