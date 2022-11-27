10b6b422240d458d2230934ae466b86008f9c52c
gashnya <ebmebm14@gmail.com>
2022-11-27 13:49:49 +0300

    aboba (3)

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main() {
     using point = bg::model::point<double, 2, bg::cs::cartesian>;
 	using box = bg::model::box<point>;
     using polygon =  bg::model::polygon<point>;
+    using linestring = bg::model::linestring<point>;
 
 	std::string test_num = "01";
 	std::string test_name = std::string("../tests/") + test_num + ".txt";
@@ -20,58 +21,67 @@ int main() {
 	double c2;
 	std::cin >> n >> c1 >> c2;
 
-	polygon poly;
+	boost::geometry::model::polygon<boost::geometry::model::point<double, 2, boost::geometry::cs::cartesian>> poly;
+    boost::geometry::model::linestring<boost::geometry::model::point<double, 2, boost::geometry::cs::cartesian>> border;
 	for (int i = 0; i < n; ++i) {
 		double x, y;
 		std::cin >> x >> y;
 		poly.outer().push_back(point(x, y));
+        border.push_back(point(x, y));
 	}
+    border.push_back(border[0]);
 
     box bbox = bg::return_envelope<box>(poly);
-    double bbox_val = 1 + bg::area(bbox) * c1;
+    double bbox_val = 1 + bg::area(bbox) * c2;
 
-    bgi::rtree<polygon, bgi::rstar<16, 4>> rtree;
+    double bbox_h = bbox.max_corner().get<1>() - bbox.min_corner().get<1>();
+    double bbox_w = bbox.max_corner().get<0>() - bbox.min_corner().get<0>();
 
-    double bbox_h = bbox.max_corner().get<1>() - bbox.max_corner().get<1>();
-    double bbox_w = bbox.max_corner().get<0>() - bbox.max_corner().get<0>();
+    double min_sum = DBL_MAX;
 
-    int w_piece_cnt = 10;
-    int h_piece_cnt = 20;
+    for (int ww = 1; ww <= bbox_w; ++ww) {
+        for (int hh = 1; hh <= bbox_h; ++hh) {
+            bgi::rtree<box, bgi::rstar<16, 4>> rtree;
 
-    double w_piece = bbox_w / w_piece_cnt;
-    double h_piece = bbox_h / h_piece_cnt;
-    double area_piece = w_piece * h_piece;
+            double w_piece = bbox_w / ww;
+            double h_piece = bbox_h / hh;
+            double area_piece = w_piece * h_piece;
 
-    point min_corner = bbox.min_corner();
-    point max_corner = min_corner;
-    bg::add_point(max_corner, point(w_piece, h_piece));
+            point min_corner = bbox.min_corner();
+            point max_corner = min_corner;
+            bg::add_point(max_corner, point(w_piece, h_piece));
 
-    for (int i = 0; i < h_piece_cnt; ++i) {
-        for (int j = 0; j < w_piece_cnt; ++j) {
-            point corner2 = min_corner;
-            bg::add_point(corner2, point(0, h_piece));
-            point corner4 = min_corner;
-            bg::add_point(corner4, point(w_piece, 0));
-            polygon p = {{min_corner, corner2, max_corner, corner4}};
-            rtree.insert(p);
+            double min_x = min_corner.get<0>();
 
-            bg::add_point(min_corner, point(w_piece, 0));
-            bg::add_point(max_corner, point(w_piece, 0));
+            std::vector<boost::geometry::model::box<boost::geometry::model::point<double, 2, boost::geometry::cs::cartesian>>> boxes;
+            for (int i = 0; i < hh; ++i) {
+                for (int j = 0; j < ww; ++j) {
+                    box b(min_corner, max_corner);
+                    rtree.insert(b);
+                    boxes.push_back(b);
+
+                    bg::add_point(min_corner, point(w_piece, 0));
+                    bg::add_point(max_corner, point(w_piece, 0));
+                }
+
+                min_corner.set<0>(min_x);
+                max_corner.set<0>(min_x + w_piece);
+
+                bg::add_point(min_corner, point(0, h_piece));
+                bg::add_point(max_corner, point(0, h_piece));
+            }
+
+            std::vector<boost::geometry::model::box<boost::geometry::model::point<double, 2, boost::geometry::cs::cartesian>>> result_i;
+            size_t intersects_cnt = rtree.query(bgi::intersects<polygon>(poly), std::back_inserter(result_i));
+
+            std::vector<boost::geometry::model::box<boost::geometry::model::point<double, 2, boost::geometry::cs::cartesian>>> result_ii;
+            size_t intersects_b_cnt = rtree.query(bgi::intersects<linestring>(border), std::back_inserter(result_ii));
+
+            min_sum = std::min(min_sum, (intersects_cnt - intersects_b_cnt) * (1 + area_piece * c1) + intersects_b_cnt * (1 + area_piece * c2));
         }
-
-        bg::add_point(min_corner, point(0, h_piece));
-        bg::add_point(max_corner, point(0, h_piece));
     }
 
-    std::vector<polygon> result_i;
-    size_t intersects_cnt = rtree.query(bgi::intersects<polygon>(poly), std::back_inserter(result_i));
-
-    std::vector<polygon> result_w;
-    size_t within_cnt = rtree.query(bgi::within<polygon>(poly), std::back_inserter(result_w));
-
-    double sum = within_cnt * (1 + area_piece * c1) + (intersects_cnt - within_cnt) * (1 + area_piece * c2);
-
-    std::cout << sum << " " << bbox_val << std::endl;
+    std::cout << min_sum << " " << bbox_val << std::endl;
 
     return 0;
 }
