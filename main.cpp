cbf841ca0a02993e7296adc1ab22a63e5bc6cb2b
sw1tcherrr <sw1tcherrr@mail.ru>
2022-11-29 01:48:36 +0300

    refactoring

--- main.cpp
+++ main.cpp
@@ -4,100 +4,103 @@
 namespace bg = boost::geometry;
 namespace bgi = boost::geometry::index;
 
-using point = bg::model::point<double, 2, bg::cs::cartesian>;
+using point = bg::model::d2::point_xy<double>;
 using box = bg::model::box<point>;
 using polygon =  bg::model::polygon<point, true, false>;
 using linestring = bg::model::linestring<point>;
 using segment = bg::model::segment<point>;
 
-double c1;
-double c2;
+void print_box(box const &b) {
+	std::cout << std::fixed << std::setprecision(9)
+			  << b.min_corner().x() << " " << b.min_corner().y() << " "
+			  << b.max_corner().x() << " " << b.max_corner().y() << "\n";
+}
 
-std::vector<box> algo(polygon poly) {
-	box bbox = bg::return_envelope<box>(poly);
-	double bbox_val = 1 + bg::area(bbox) * c2;
-
-	linestring border;
-	for (auto&& i : poly.outer()) {
-		border.push_back(i);
+double score(std::vector<box> const& inside, std::vector<box> const& crossing, double c1, double c2) {
+	double res = 0;
+	for (auto&& b : inside) {
+		res += 1 + bg::area(b) * c1;
 	}
-	border.push_back(border[0]);
+	for (auto&& b : crossing) {
+		res += 1 + bg::area(b) * c2;
+	}
+	return res;
+}
 
-	double bbox_h = bbox.max_corner().get<1>() - bbox.min_corner().get<1>();
-	double bbox_w = bbox.max_corner().get<0>() - bbox.min_corner().get<0>();
+std::vector<box> equal_boxes(box const& bbox, int w_cnt, int h_cnt) {
+	double bbox_w = bbox.max_corner().x() - bbox.min_corner().x();
+	double bbox_h = bbox.max_corner().y() - bbox.min_corner().y();
+	double piece_w = bbox_w / w_cnt;
+	double piece_h = bbox_h / h_cnt;
 
-	double min_sum = bbox_val;
-	std::vector<box> min_vec;
+	point min_corner = bbox.min_corner();
+	point max_corner(min_corner.x() + piece_w, min_corner.y() + piece_h);
+	point hor_shift(piece_w, 0);
+	point vert_shift(0, piece_h);
+	double min_x = bbox.min_corner().x();
 
-//	std::cout << bbox_w << " " << bbox_h << std::endl;
-	for (int ww = 1; ww <= 100; ww += 5) {
-		for (int hh = 1; hh <= 100; hh += 5) {
-			bgi::rtree<box, bgi::rstar<16, 4>> rtree;
+	std::vector<box> boxes;
+	for (int i = 0; i < h_cnt; ++i) {
+		for (int j = 0; j < w_cnt; ++j) {
+			boxes.emplace_back(min_corner, max_corner);
 
-			double w_piece = bbox_w / ww;
-			double h_piece = bbox_h / hh;
-			double area_piece = w_piece * h_piece;
-
-			point min_corner = bbox.min_corner();
-			point max_corner = min_corner;
-			bg::add_point(max_corner, point(w_piece, h_piece));
-
-			double min_x = min_corner.get<0>();
-
-			std::vector<boost::geometry::model::box<boost::geometry::model::point<double, 2, boost::geometry::cs::cartesian>>> boxes;
-			for (int i = 0; i < hh; ++i) {
-				for (int j = 0; j < ww; ++j) {
-					box b(min_corner, max_corner);
-					rtree.insert(b);
-					boxes.push_back(b);
-
-					bg::add_point(min_corner, point(w_piece, 0));
-					bg::add_point(max_corner, point(w_piece, 0));
-				}
-
-				min_corner.set<0>(min_x);
-				max_corner.set<0>(min_x + w_piece);
-
-				bg::add_point(min_corner, point(0, h_piece));
-				bg::add_point(max_corner, point(0, h_piece));
-			}
-
-			std::vector<boost::geometry::model::box<boost::geometry::model::point<double, 2, boost::geometry::cs::cartesian>>> result_i;
-			size_t intersects_cnt = rtree.query(bgi::intersects<polygon>(poly), std::back_inserter(result_i));
-
-			std::vector<boost::geometry::model::box<boost::geometry::model::point<double, 2, boost::geometry::cs::cartesian>>> result_ii;
-			size_t intersects_b_cnt = rtree.query(bgi::intersects<linestring>(border), std::back_inserter(result_ii));
-
-			double cur = (intersects_cnt - intersects_b_cnt) * (1 + area_piece * c1) + intersects_b_cnt * (1 + area_piece * c2);
-			if (cur < min_sum) {
-				min_sum = cur;
-				min_vec = std::move(result_i);
-			}
+			bg::add_point(min_corner, hor_shift);
+			bg::add_point(max_corner, hor_shift);
 		}
+		min_corner.x(min_x);
+		bg::add_point(min_corner, vert_shift);
+		max_corner.x(min_x + piece_w);
+		bg::add_point(max_corner, vert_shift);
 	}
-	std::cout << min_sum << std::endl;
-	return min_vec;
+
+	return boxes;
+}
+
+//std::vector<box> vertex_boxes() {
+//
+//}
+
+struct inside_predicate {
+	polygon const& poly;
+	linestring const& border;
+
+	template <class Value>
+	bool operator()(Value const& v) const {
+		return bg::intersects(v, poly) && !bg::intersects(v, border);
+	}
+};
+
+std::pair<std::vector<box>, std::vector<box>> filter_boxes(polygon const& poly, linestring const& border, std::vector<box>&& boxes) {
+	bgi::rtree<box, bgi::rstar<16, 4>> rtree(boxes);
+
+	std::vector<box> inside;
+	rtree.query(bgi::satisfies(inside_predicate{poly, border}), std::back_inserter(inside));
+
+	std::vector<box> crossing;
+	rtree.query(bgi::intersects(border), std::back_inserter(crossing));
+
+	return {std::move(inside), std::move(crossing)};
 }
 
 std::vector<polygon> split_poly(polygon poly) {
-	box bbox = bg::return_envelope<box>(poly);
+	auto bbox = bg::return_envelope<box>(poly);
 	point top, r, bot, l;
 	int ti, ri, bi, li;
 	for (int i = 0; i < poly.outer().size(); ++i) {
 		auto p = poly.outer()[i];
-		if (p.get<1>() == bbox.max_corner().get<1>()) {
+		if (p.y() == bbox.max_corner().y()) {
 			top = p;
 			ti = i;
 		}
-		if (p.get<1>() == bbox.min_corner().get<0>()) {
+		if (p.y() == bbox.min_corner().x()) {
 			bot = p;
 			bi = i;
 		}
-		if (p.get<0>() == bbox.min_corner().get<0>()) {
+		if (p.x() == bbox.min_corner().x()) {
 			l = p;
 			li = i;
 		}
-		if (p.get<0>() == bbox.max_corner().get<0>()) {
+		if (p.x() == bbox.max_corner().x()) {
 			r = p;
 			ri = i;
 		}
@@ -118,13 +121,11 @@ std::vector<polygon> split_poly(polygon poly) {
 	for (int i = li; i <= ti; ++i) {
 		tl.outer().push_back(poly.outer()[i]);
 	}
-//	std::vector<point> pv;
-//	bg::intersection(segment({top, bot}), segment({l, r}), pv);
-//	auto p = pv[0];
-	double bbox_h = bbox.max_corner().get<1>() - bbox.min_corner().get<1>();
-	double bbox_w = bbox.max_corner().get<0>() - bbox.min_corner().get<0>();
 
-	point p(bbox.min_corner().get<0> ()+ bbox_w / 2, bbox.min_corner().get<1>() + bbox_h / 2);
+	double bbox_h = bbox.max_corner().y() - bbox.min_corner().y();
+	double bbox_w = bbox.max_corner().x() - bbox.min_corner().x();
+
+	point p(bbox.min_corner().get<0> ()+ bbox_w / 2, bbox.min_corner().y() + bbox_h / 2);
 	tr.outer().push_back(p);
 	br.outer().push_back(p);
 	bl.outer().push_back(p);
@@ -133,35 +134,74 @@ std::vector<polygon> split_poly(polygon poly) {
 	return {tr, br, bl, tl};
 }
 
+struct result {
+	std::vector<box> inside;
+	std::vector<box> crossing;
+	double score{0};
+};
+
+struct brute_force_eq_result : result {
+	int w_cnt{-1};
+	int h_cnt{-1};
+};
+
+brute_force_eq_result brute_force_eq(polygon const& poly, double c1, double c2, int range = 100, int step = 1) {
+	box bbox = bg::return_envelope<box>(poly);
+	double bbox_score = 1 + bg::area(bbox) * c2;
+
+	linestring border(poly.outer().begin(), poly.outer().end());
+	border.push_back(border[0]);
+	bg::correct(border);
+
+	brute_force_eq_result res;
+	res.score = bbox_score;
+
+	for (int w_cnt = 1; w_cnt <= range; w_cnt += step) {
+		for (int h_cnt = 1; h_cnt <= range; h_cnt += step) {
+			auto [inside, crossing] = filter_boxes(poly, border, equal_boxes(bbox, w_cnt, h_cnt));
+
+			double cur_score = score(inside, crossing, c1, c2);
+			if (cur_score < res.score) {
+				res.inside = std::move(inside);
+				res.crossing = std::move(crossing);
+				res.score = cur_score;
+				res.w_cnt = w_cnt;
+				res.h_cnt = h_cnt;
+			}
+		}
+	}
+
+	return res;
+}
+
 int main() {
-	std::string test_num = "02";
-	std::string test_name = std::string("../tests/") + test_num + ".txt";
-	std::string res_name = std::string("../") + test_num + ".txt";
-	std::freopen(test_name.data(), "r", stdin);
-    std::freopen(res_name.data(), "w", stdout);
+	#define TEST_NUM "02"
+	char const* test_name = "../tests/" TEST_NUM ".txt";
+	char const* res_name = "../" TEST_NUM ".txt";
+	#undef TEST_NUM
+
+	if (!std::freopen(test_name, "r", stdin)) {return -1;}
+    if (!std::freopen(res_name, "w", stdout)) {return -1;}
 
 	int n;
+	double c1, c2;
 	std::cin >> n >> c1 >> c2;
 
 	polygon poly;
 	for (int i = 0; i < n; ++i) {
 		double x, y;
 		std::cin >> x >> y;
-		poly.outer().push_back(point(x, y));
+		poly.outer().emplace_back(x, y);
 	}
+	bg::correct(poly);
 
-	std::vector<box> min_vec;
-	auto polys = split_poly(poly);
-	for (auto&& p : polys) {
-		for (auto&& b : algo(p)) {
-			min_vec.push_back(b);
-		}
+	auto res = brute_force_eq(poly, c1, c2);
+
+	std::cout << res.inside.size() + res.crossing.size() << "\n";
+	for (auto&& b : res.inside) {
+		print_box(b);
 	}
-
-	std::cout << min_vec.size() << "\n";
-	for (auto&& b : min_vec) {
-		std::cout << std::fixed << std::setprecision(9) << b.min_corner().get<0>() << " " << b.min_corner().get<1>() << " " << b.max_corner().get<0>() << " " << b.max_corner().get<1>() << "\n";
+	for (auto&& b : res.crossing) {
+		print_box(b);
 	}
-
-    return 0;
 }
