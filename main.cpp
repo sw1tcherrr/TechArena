35db1788a22946e2740f4c1a8a728025b3074d17
gashnya <ebmebm14@gmail.com>
2022-11-28 18:44:29 +0300

    initial xy-partitioning + cropping

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main() {
     using polygon =  bg::model::polygon<point>;
     using linestring = bg::model::linestring<point>;
 
-	std::string test_num = "03";
+	std::string test_num = "01";
 	std::string test_name = std::string("../tests/") + test_num + ".txt";
 	std::string res_name = std::string("../") + test_num + ".txt";
 	std::freopen(test_name.data(), "r", stdin);
@@ -21,15 +21,25 @@ int main() {
 	double c2;
 	std::cin >> n >> c1 >> c2;
 
+    std::set<double> x_points;
+    std::set<double> y_points;
+
 	boost::geometry::model::polygon<boost::geometry::model::point<double, 2, boost::geometry::cs::cartesian>> poly;
     boost::geometry::model::linestring<boost::geometry::model::point<double, 2, boost::geometry::cs::cartesian>> border;
 	for (int i = 0; i < n; ++i) {
 		double x, y;
 		std::cin >> x >> y;
+
 		poly.outer().push_back(point(x, y));
         border.push_back(point(x, y));
+
+        x_points.insert(x);
+        y_points.insert(y);
 	}
     border.push_back(border[0]);
+
+    bg::correct(poly);
+    bg::correct(border);
 
     box bbox = bg::return_envelope<box>(poly);
     double bbox_val = 1 + bg::area(bbox) * c2;
@@ -37,61 +47,67 @@ int main() {
     double bbox_h = bbox.max_corner().get<1>() - bbox.min_corner().get<1>();
     double bbox_w = bbox.max_corner().get<0>() - bbox.min_corner().get<0>();
 
-    double min_sum = bbox_val;
-	std::vector<box> min_vec;
+    bgi::rtree<box, bgi::rstar<16, 4>> rtree;
 
-    for (int ww = 1; ww <= bbox_w; ++ww) {
-        for (int hh = 1; hh <= bbox_h; ++hh) {
-            bgi::rtree<box, bgi::rstar<16, 4>> rtree;
+    std::set<double>::iterator x_it;
+    std::set<double>::iterator y_it;
 
-            double w_piece = bbox_w / ww;
-            double h_piece = bbox_h / hh;
-            double area_piece = w_piece * h_piece;
+    std::vector<boost::geometry::model::box<boost::geometry::model::point<double, 2, boost::geometry::cs::cartesian>>> boxes;
 
-            point min_corner = bbox.min_corner();
-            point max_corner = min_corner;
-            bg::add_point(max_corner, point(w_piece, h_piece));
+    for (y_it = y_points.begin(); y_it != y_points.end(); ++y_it) {
+        for (x_it = x_points.begin(); x_it != x_points.end(); ++x_it) {
+            std::set<double>::iterator next_x_it = std::next(x_it);
+            std::set<double>::iterator next_y_it = std::next(y_it);
 
-            double min_x = min_corner.get<0>();
-
-            std::vector<boost::geometry::model::box<boost::geometry::model::point<double, 2, boost::geometry::cs::cartesian>>> boxes;
-            for (int i = 0; i < hh; ++i) {
-                for (int j = 0; j < ww; ++j) {
-                    box b(min_corner, max_corner);
-                    rtree.insert(b);
-                    boxes.push_back(b);
-
-                    bg::add_point(min_corner, point(w_piece, 0));
-                    bg::add_point(max_corner, point(w_piece, 0));
-                }
-
-                min_corner.set<0>(min_x);
-                max_corner.set<0>(min_x + w_piece);
-
-                bg::add_point(min_corner, point(0, h_piece));
-                bg::add_point(max_corner, point(0, h_piece));
+            if (next_x_it == x_points.end() || next_y_it== y_points.end()) {
+                continue;
             }
 
-            std::vector<boost::geometry::model::box<boost::geometry::model::point<double, 2, boost::geometry::cs::cartesian>>> result_i;
-            size_t intersects_cnt = rtree.query(bgi::intersects<polygon>(poly), std::back_inserter(result_i));
-
-            std::vector<boost::geometry::model::box<boost::geometry::model::point<double, 2, boost::geometry::cs::cartesian>>> result_ii;
-            size_t intersects_b_cnt = rtree.query(bgi::intersects<linestring>(border), std::back_inserter(result_ii));
-
-			double cur = (intersects_cnt - intersects_b_cnt) * (1 + area_piece * c1) + intersects_b_cnt * (1 + area_piece * c2);
-            if (cur < min_sum) {
-				min_sum = cur;
-				min_vec = std::move(result_i);
-			}
+            box b(point(*x_it, *y_it), point(*next_x_it, *next_y_it));
+            rtree.insert(b);
+            boxes.push_back(b);
         }
     }
 
-    std::cout << min_sum << " " << bbox_val << std::endl;
+    std::vector<boost::geometry::model::box<boost::geometry::model::point<double, 2, boost::geometry::cs::cartesian>>> result_i;
+    size_t intersects_cnt = rtree.query(bgi::intersects<polygon>(poly), std::back_inserter(result_i));
 
-	std::cout << min_vec.size() << "\n";
-	for (auto&& b : min_vec) {
-		std::cout << std::fixed << std::setprecision(9) << b.min_corner().get<0>() << " " << b.min_corner().get<1>() << " " << b.max_corner().get<0>() << " " << b.max_corner().get<1>() << "\n";
-	}
+    std::vector<boost::geometry::model::box<boost::geometry::model::point<double, 2, boost::geometry::cs::cartesian>>> result_ii;
+    size_t intersects_b_cnt = rtree.query(bgi::intersects<linestring>(border), std::back_inserter(result_ii));
+
+    auto res_it = std::begin(result_ii);
+    while(res_it != std::end(result_ii)) {
+        box b = *res_it;
+
+        std::vector<polygon> p;
+        bg::intersection(poly, b, p);
+
+        if (p.size() == 0) {
+            result_ii.erase(res_it);
+            continue;
+        }
+
+        if (p.size() > 0) {
+            box bbox_p = bg::return_envelope<box>(p[0]);
+
+            double p_area = bg::area(p[0]);
+            double bbp_area = bg::area(bbox_p);
+
+            if (std::abs(p_area - bbp_area) < 0.000000001) {
+                result_ii.erase(res_it);
+                continue;
+            }
+
+            *res_it = std::move(bbox_p);
+        }
+
+        ++res_it;
+    }
+
+	// std::cout << min_vec.size() << "\n";
+	// for (auto&& b : min_vec) {
+	// 	std::cout << std::fixed << std::setprecision(9) << b.min_corner().get<0>() << " " << b.min_corner().get<1>() << " " << b.max_corner().get<0>() << " " << b.max_corner().get<1>() << "\n";
+	// }
 
     return 0;
 }
